45c316e636b097d6ac50e2af813946d7e24dd0e3
Vladimir Ivanov <luvolunov@gmail.com>
2022-11-05 19:31:59 +0900

    Initial Commit

---
+++ Labs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Labs
+//
+//  Created by Vladimir Ivanov on 05.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
